8e594057bed2b54cb48aea341d51d262aa6c10ee
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-04-24 18:25:26 +0700

    add MongoDB connection settings in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using ActivitiesApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
+if (connectionString == null)
+{
+    Console.WriteLine("You must set your 'MONGODB_URI' environment variable. To learn how to set it, see https://www.mongodb.com/docs/drivers/csharp/current/quick-start/#set-your-connection-string");
+    Environment.Exit(0);
+}
+var config = builder.Configuration.GetSection("ActivityDatabaseSettings").Get<ActivityDatabaseSettings>();
+config!.ConnectionString = connectionString;
+builder.Services.Configure<ActivityDatabaseSettings>(i =>
+{
+    i.ConnectionString = config.ConnectionString;
+    i.DatabaseName = config.DatabaseName;
+    i.ActivitiesCollectionName = config.ActivitiesCollectionName;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
